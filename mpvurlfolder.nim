92ddc0df23c4bf2ce88a0e73af58b6a8d9c0e2dd
Egor Scherbakov <50289495+egor4ka@users.noreply.github.com>
2022-01-30 21:57:01 +0200

    Add files via upload

---
+++ mpvurlfolder.nim
@@ -0,0 +1,28 @@
+#import the necessary modules
+import os, strformat, htmlparser, strtabs, xmltree
+
+#declare variables
+let 
+  url = commandLineParams()[0]
+  file = "input.html"
+var musicFiles = newSeq[string]()
+
+discard execShellCmd(fmt"curl -o {file} {url}")
+
+#declare the html variable because it must be ran after the curl
+let html = loadHtml(file)
+
+#loop over the hrefs
+for a in html.findAll("a"):
+  if a.attrs.hasKey "href":
+    musicFiles.add(a.attrs["href"])
+
+#delete the ../ thingy
+musicFiles.del(0)
+
+#loop over the files and play them
+for i in musicFiles:
+  discard execShellCmd(fmt"mpv {url}{i}")
+
+#remove the curled file
+removeFile(file)
